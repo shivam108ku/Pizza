a6c73d58266572b70a2576907a8d41f5ffc6945a
Shivam Kumar <shivam108ku@gmail.com>
2025-07-04 12:48:53 +0530

    error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import logger from './config/logger';
+import createHttpError, { HttpError } from 'http-errors';
+import { NextFunction } from 'connect';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Welcome Shivam');
+app.get('/', (req, res, next) => {
+    const err = createHttpError(401, 'Cant access');
+    next(err);
+    // res.send('Welcome Shivam');
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    });
 });
 
 export default app;
