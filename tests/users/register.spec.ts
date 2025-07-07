109d608e17c927a86657162f93cf32656a4f512b
Shivam Kumar <shivam108ku@gmail.com>
2025-07-07 15:45:25 +0530

    add new test

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -22,6 +22,26 @@ describe('POST /auth/register', () => {
             // Assert
             expect(response.statusCode).toBe(201);
         });
+
+        it('should return valis json repsosne', async () => {
+            // AAA
+
+            // Arrange
+            const userData = {
+                firstName: 'Shivam',
+                lstName: 'K',
+                email: 'shivamn@gmail.com',
+                password: 'secret',
+            };
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData);
+            // Assert
+            expect(
+                (response.headers as Record<string, string>)['content-type'],
+            ).toEqual(expect.stringContaining('json'));
+        });
     });
 
     describe('Felds are missing', () => {});
